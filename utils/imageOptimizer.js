291ba368ac9d358ccd3eb1b4a725f6e992015284
Gideon Kwarteng <kwarteon08@gmail.com>
2026-06-02 16:12:10 +0000

    Fix Cloudinary images: use q_80 instead of q_auto; add SafeImage component with retry

--- utils/imageOptimizer.js
+++ utils/imageOptimizer.js
@@ -3,12 +3,12 @@ function cloudinaryUrl(url, width = 800) {
 
   const { CLOUDINARY_CLOUD_NAME } = process.env;
   if (CLOUDINARY_CLOUD_NAME && !url.startsWith('http://') && !url.startsWith('https://')) {
-    return `https://res.cloudinary.com/${CLOUDINARY_CLOUD_NAME}/image/upload/w_${width},q_auto,f_auto/v1/${url}`;
+    return `https://res.cloudinary.com/${CLOUDINARY_CLOUD_NAME}/image/upload/w_${width},q_80,f_auto/v1/${url}`;
   }
 
   return url
-    .replace(/(\/upload\/)(?:w_[^/]+,q_auto,f_auto\/)+/, '$1')
-    .replace('/upload/', `/upload/w_${width},q_auto,f_auto/`);
+    .replace(/(\/upload\/)(?:w_[^/]+,q_(?:auto|\d+),f_auto\/)+/, '$1')
+    .replace('/upload/', `/upload/w_${width},q_80,f_auto/`);
 }
 
 module.exports = { cloudinaryUrl };
