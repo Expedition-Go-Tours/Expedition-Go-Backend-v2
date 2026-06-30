bd164635e37f180077ac1e8de70b5ca5bfadf571
Gideon Kwarteng <kwarteon08@gmail.com>
2026-06-30 13:01:00 +0000

    fix: add missing Stripe, getConfig, and transaction mocks to resolve test failures

--- providers/geoapifyProvider.js
+++ providers/geoapifyProvider.js
@@ -1,5 +1,4 @@
 const axios = require('axios');
-const logger = require('../utils/logger');
 
 const BASE_URL = 'https://api.geoapify.com/v1/geocode';
 
--- providers/nominatimProvider.js
+++ providers/nominatimProvider.js
@@ -1,5 +1,4 @@
 const axios = require('axios');
-const logger = require('../utils/logger');
 
 const BASE_URL = 'https://nominatim.openstreetmap.org';
 
