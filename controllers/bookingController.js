9ab50b31f8183205f0c34b2f4da45bcddd78c465
gideon211 <kwarteon08@gmail.com>
2026-08-28 14:02:39 +0000

    test: trigger ci failure for ai-debug v4

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1524,3 +1524,6 @@ exports.updateBookingStatus = catchAsync(async (req, res, next) => {
 module.exports = exports;
 // CI TEST DELETE ME
 throw new Error('deliberate failure');
+
+// CI TEST DELETE ME
+throw new Error('deliberate failure');
