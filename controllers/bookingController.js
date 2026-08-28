31100c2bbc69a0f8faa3bb050b11ea1cb1511e63
gideon211 <kwarteon08@gmail.com>
2026-08-28 15:01:07 +0000

    test: trigger ci failure for artifact capture

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1522,3 +1522,6 @@ exports.updateBookingStatus = catchAsync(async (req, res, next) => {
 // ================================
 
 module.exports = exports;
+
+// CI TEST DELETE ME
+throw new Error('deliberate failure');
